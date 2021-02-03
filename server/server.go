fd93b2d3ab0c46e6e04829dfbe09ca6e5b1abfb4
juarezbarneond44 <69942635+juarezbarneond44@users.noreply.github.com>
2021-02-03 17:55:32 -0600

    Create server.go

---
+++ server/server.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
+)
+
+type Ram struct {
+	Total      string `json:"total"`
+	Porcentaje string `json:"porcentaje"`
+	Usado      string `json:"usado"`
+}
+
+type Cpu struct {
+	Nucleo1 string `json:"nucleo1"`
+	Libre1  string `json:"libre1"`
+}
+//funcion que realizara los datos de la ram
+func homeRAM(w http.ResponseWriter, req *http.Request) {
+	enableCors(&w) // habilitamos cors
+	//var Datoram Ram
+
+	println("******** Cargar Datos RAM******")
+	println("201700556")
+
+	// funcion para obtener datos de la ram	 
+/*
+// de esta manera se obtienen los valores del archivo memo 
+b, err := ioutil.ReadFile("/proc/memo_201700556") // obtenemos el archivo
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b) // convert content to a 'string'
+	//fmt.Println(str) // print the content as a 'string'
+	json.NewEncoder(w).Encode(str)
+*/
+	
+	// se utilizo mem.virtualmemory()   ya que los valores del archivo memo no mostraron valores exactos
+	v, _ := mem.VirtualMemory()
+	var Total = v.Total / (1024 * 1024)                  //valor total de ram en mb
+	var Porcentaje = v.UsedPercent                       // porcentaje de utiliacion
+	var used = uint64(float64(Total) * Porcentaje / 100) // cantidad de memoria utilizada
+
+	datos := Ram{
+		Total:      fmt.Sprintf("%v", Total),
+		Porcentaje: fmt.Sprintf("%f", Porcentaje),
+		Usado:      fmt.Sprintf("%v", used),
+	}
+
+	b, err := json.MarshalIndent(datos, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// valores de la ram en consola
+	fmt.Print(string(b))
+
+	// enviamos en formato json los datos de la ram mediante peticion http
+	json.NewEncoder(w).Encode(datos)
+
+}
+func homeCPU(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	println("******** Cargar Datos CPU******")
+	
+	//w.Write([]byte("Datos Del CPU")
+
+	percent, _ := cpu.Percent(time.Second, true)
+	var nucleo1 = percent[0] // obtenemos el uso del procesador
+	var libre1 = 100 - nucleo1 // obtenemos el espacio libre del procesador
+	// si se tiene mas de 1 nucleo se puede poner de la siguiente manera 
+	//percent[1],percent[2],percent[n]   
+	fmt.Printf("nucleo 1: %v\n", nucleo1) 
+	fmt.Printf("libre 1: %v,\n", libre1)
+	dato := Cpu{
+		Nucleo1: fmt.Sprintf("%v", nucleo1),
+		Libre1:  fmt.Sprintf("%v", libre1),
+	}
+	b, err := json.MarshalIndent(dato, "", "  ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	// valores en consola
+	fmt.Print(string(b))
+	// enviamos en formato json los datos del cpu mediante peticion http
+	json.NewEncoder(w).Encode(dato)
+}
+func datosCPU(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	println("******** Cargar Datos CPU******")
+
+	b, err := ioutil.ReadFile("/proc/cpu_201700556") // obtenemos el archivo
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b) // convert content to a 'string'
+
+	//fmt.Println(str) // print the content as a 'string'
+
+	json.NewEncoder(w).Encode(str)
+}
+
+func main() {
+	router := mux.NewRouter()
+  s := router.Host("192.168.56.1").Subrouter()
+	s.HandleFunc("/Ram", homeRAM).Methods("GET")
+	s.HandleFunc("/DatoCpu", datosCPU).Methods("GET")
+	s.HandleFunc("/Cpu", homeCPU).Methods("GET")
+	// levantamos el servidor en el puerto 4444 
+	log.Fatal(http.ListenAndServe(":4444", s))
+}
+
+// esta funcion sirve para poder mandar peticiones a angular ya que habilita los cors
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+}
