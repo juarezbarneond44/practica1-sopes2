962e781999f05f24b7e7888fa8092f789ffe2b86
Juan Carlos Juarez Barneond <2986841360101@ingenieria.usac.edu.gt>
2021-05-05 00:21:00 -0600

    f

--- FrontEnd/src/Servicios/backend.service.ts
+++ FrontEnd/src/Servicios/backend.service.ts
@@ -11,18 +11,18 @@ export class BackendService {
 headers:HttpHeaders=new HttpHeaders({"Content-Type":"application/json"})
 
 GetRam(){
-const url="http://192.168.1.21:4444/Ram"
+const url="http://35.223.64.146:4444/Ram"
  
 return this.http.get(url)
 }
 GetCpu(){
-  const url="http://192.168.1.21:4444/Cpu"
+  const url="http://35.223.64.146:4444/Cpu"
    
   return this.http.get(url)
   }
 
   KillProcess(id:string){
-    const url="http://192.168.1.21:4444/kill/"+id
+    const url="http://35.223.64.146:4444/kill/"+id
    
       return  this.http.get(url)
    
@@ -32,7 +32,7 @@ GetCpu(){
 
 
 GetProceso(){
-  const url="http://192.168.1.21:4444/DatoCpu"
+  const url="http://35.223.64.146:4444/DatoCpu"
    
   return this.http.get(url)
   }
